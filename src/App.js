234212a49de0c96021e0c2320996bafcaa09bd27
amolsr <amol.saini567@gmail.com>
2021-01-28 01:39:07 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "./components/Appbar";
+import "./App.css";
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Typography,
+  useTheme,
+} from "@material-ui/core";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/image/background1.jpg)`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    padding: theme.spacing(0, 5),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classes.root}>
+      <AppBar>
+        <Grid
+          container
+          style={{ height: "93vh" }}
+          direction="column"
+          justify="center"
+          alignItems="flex-start"
         >
-          Learn React
-        </a>
-      </header>
+          <Grid item xs={13}>
+            <Typography
+              variant="h3"
+              align="left"
+              style={{ color: theme.palette.primary.main }}
+              gutterBottom
+            >
+              Amol Saini
+            </Typography>
+          </Grid>
+          <Grid item xs={13}>
+            <List dense="true">
+              <ListItem>
+                <ListItemIcon>
+                  <PhoneIcon />
+                </ListItemIcon>
+                <ListItemText primary="8126544009" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <EmailIcon />
+                </ListItemIcon>
+                <ListItemText primary="amol.saini567@gmail.com" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Ghaziabad, Uttar Pradesh, India" />
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
+      </AppBar>
     </div>
   );
 }
